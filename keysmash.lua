876b9792c832b1a0b94ae65dc9e15759fd29b5d5
Mock <matheus.mtb7@gmail.com>
2022-09-17 20:20:09 -0300

    Updating the code and describing the reason

--- keysmash.lua
+++ keysmash.lua
@@ -1,12 +1,16 @@
 function isKeySmash(text)
 	text = text:lower()
-	local common = "[asdfghjklç]"
+	local common = "[asdfghjklç0-9]"
 	local uncommon = "[zxcqwem,%.uio]"
 	local laught = "[ahus]" --Ppl lught wih: shusauhashuhsuauhashusa
 	local score = 0
 
 	local keysSmashLetters = 0
-	local vogals = '[a]'
+	local vogals = '[aeiou]'
+
+	if text:match("[%[%]%(%)%%0-9;!%?@\\|/]") then 
+		return false, 0
+	end
 
 	local segmentVogals = 0
 	local lastIsVogal = false
@@ -49,26 +53,41 @@ function isKeySmash(text)
 	local treshhold = #text*(1 - 0.15)
 	
 	if text:match("http.+") or (laughtLenght >= treshhold) then
-		return
+		return false, score, keysSmashLetters
 	end
 	
+	local reason = ""
 
 	--print(text, keysSmashLetters.."/"..(#text), segmentVogals, "Tresh: "..treshhold)
+	local range = math.max((#text-math.max(1, treshhold)), 8)
 	if keysSmashLetters > 8 and keysSmashLetters >= (#text-math.max(1, treshhold)) then
 		score = score +1
+	else
+		reason = "not enoght score (<"..range..");"
+		return false, reason, keysSmashLetters
 	end
 	if segmentVogals > 0 then
 		score = score +1
+	else 
+		reason = reason .. "too many vowels (vowel score: "..segmentVogals..");"
 	end
+
+	local letter_score = 0
 
 	for i,b in pairs(letterMap) do 
 		if b >= 0.7 * #text then 
 			score = score -1
+			reason = reason .. "repeat "..i.." too many times ("..tostring(0.7 * #text)..");"
 		end
 	end
 
-	if score > 1 then 
-		return true
+	if text:find("çã") or text:find("çõ") then 
+		score = score - #text/2
+		reason = reason .. "found çã and çõ;"
 	end
-	return false
+
+	if score > 1 then 
+		return true, reason.."ok", keysSmashLetters
+	end
+	return false, reason, keysSmashLetters
 end
