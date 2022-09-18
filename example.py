560d2ae5a6b749eff54c3f99798449e670d955f6
jrrhack <50176581+jrrhack@users.noreply.github.com>
2022-09-18 16:58:59 -0300

    Made my python version
    It works with 3 inputs isKeySmash(string, int repetition, int vowel)
    
    string: the string to be evaluated
    
    int repetition: integer with the maximum percentage of repeated letters that is considered KeySmash (33% seems to work fine) If theres more than 33% of repeated letters in a word its KeySmash.
    
    int vowel:  integer with the maximum percentage of vowels letters that is considered KeySmash (25% - 33% seems to work fine). If theres less than 33% of vowels in a word its probably a KeySmash, but that´s not always true since Vowels are distributes evenly across the keyboard

---
+++ example.py
@@ -0,0 +1,13 @@
+from functions import *
+# %%
+print(isKeySmash("cake", 33, 33))
+print(isKeySmash("useful", 33, 33))
+print(isKeySmash("pneumonoultramicroscopicsilicovolcanoconiosis", 33, 33))
+print(isKeySmash("aeuhehuaehauehuaehaueh", 33, 33))
+print(isKeySmash("KOPASKSPOAKSPOKSOPAKSPOAKSPOA", 33, 33))
+print(isKeySmash("klashjdlkjsdhfjklsdh", 33, 33))
+print(isKeySmash("ASLKDJSALKDJASLKDJASLK", 33, 33))
+print(isKeySmash("goodnessalsdgkwnlzkgjkssd", 33, 33))
+print(isKeySmash("AKXZNVJLKKSALDFKALD", 33, 33))
+print(isKeySmash("AKXFDJSKAHFKLDHAZSJAGQKJDHFJKS", 33, 33))
+print(isKeySmash("YESSSHFDHQJKLDSHGJXSFG", 33, 33))
